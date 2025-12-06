298283b918c5c991df377441552dc5efbccd24d0
Sanjai <sanjaigukesan8686@gmail.com>
2025-12-06 15:42:54 +0530

    Delete flutter_application_1 directory

--- flutter_application_1/lib/utils/friendly_date.dart
+++
@@ -1,9 +0,0 @@
-String friendlyDate(DateTime date) {
-final diff = DateTime.now().difference(date);
-if (diff.inDays >= 365) return '${(diff.inDays / 365).floor()} yr ago';
-if (diff.inDays >= 30) return '${(diff.inDays / 30).floor()} mo ago';
-if (diff.inDays >= 1) return '${diff.inDays} days ago';
-if (diff.inHours >= 1) return '${diff.inHours} hrs ago';
-if (diff.inMinutes >= 1) return '${diff.inMinutes} mins ago';
-return 'just now';
-}
